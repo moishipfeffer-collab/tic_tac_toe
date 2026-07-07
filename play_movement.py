0b514cfc17e7c4942e6b9b10a3cdd0894759f359
david <d8087869@gmail.com>
2026-07-07 12:24:25 +0300

    final chenges let me know if is good , thx

--- play_movement.py
+++ play_movement.py
@@ -5,11 +5,11 @@ def get_index():
     return row, column
 
 def is_free(board,row,column):
-    if board[row][column] == "-":
+    if board[row-1][column-1] == "-":
         return True
     else:
         False
 
 def mark_board(board, row,column ,symbol):
-    board[row][column] = symbol
+    board[row-1][column-1] = symbol
     return board
