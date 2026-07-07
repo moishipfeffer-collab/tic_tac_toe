7c02d8d4aeb4f4db259c3425b0bcc96928f5fd64
yaakov-ai <ysoibelman@gmail.com>
2026-07-07 12:27:03 +0300

    I love Hashem

--- board.py
+++ board.py
@@ -12,12 +12,12 @@ def print_board (board:list):
 def input_explain ():
     print (" to play, please enter the index of row and column as seen in the picture: you would like to put your symbol")
     print (""" 
-    columnn 1  2  3
+        columnn 
+            1  2  3
     Row 1   -  -  -
         2   -  -  -
         3   -  -  -
            """)
     
 board = create_board()
-print_board(board)
 input_explain()
