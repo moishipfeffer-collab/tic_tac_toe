af7ba7df2a50d3297fd4ae0b61a2b18f136a0c02
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-07 11:55:32 +0300

    sum fixes

--- board_check.py
+++ board_check.py
@@ -1,13 +1,14 @@
-def check_board(board):
+def check_board_row(board):
     for x in board:
         if  x[0]==x[1]==x[2]:
-            print("win!")
+            return True
+def check_board_column(board):
     for y in range(3):
-        if board[0][y]==board[1][y] and board[0][y]==board[2][y]:
-            print("win!")
+        if board[0][y]==board[1][y]==board[2][y]:
+            return True
+def check_board_full(board):
     if not " " in board:
         print("the board is full, game over")
     
     
 
-check_board([[0,1,0],[1,0,0],[1,1,0]])
