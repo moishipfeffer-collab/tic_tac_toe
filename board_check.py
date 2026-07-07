4c4d706e1902d1cf5866aa65a0674dadeca6980e
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-07 14:37:24 +0300

    asdkjh

--- board_check.py
+++ board_check.py
@@ -1,21 +1,15 @@
 def check_board_row(board):
     for x in board:
-        if  x[0]==x[1]==x[2]:
+        if  x[0]==x[1]==x[2] and x[0] !="-":
             return True
 def check_board_column(board):
     for y in range(3):
-        if board[0][y]==board[1][y]==board[2][y]:
+        if board[0][y]==board[1][y]==board[2][y] and board[0][y] != "-":
             return True
-def check_board_full(board):
-    if "-" not in board:
-        return True
 
-def boards_outputs():
-    if check_board_row:
-        print("you won!")
-    elif check_board_column:
-        print("you won!")
-    elif check_board_full:
-        print("board is full, game ovar!")
-    
+def boards_outputs(board):
+    if check_board_row(board):
+        print("You Won!\nGame Over!" )
+    elif check_board_column(board):
+        print("You Won!\nGame Over!")
 
