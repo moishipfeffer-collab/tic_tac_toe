36dce9ebf95b4f4f75ae157a91b7a713ae21e154
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-08 11:18:01 +0300

    choose sign added

--- manag_game.py
+++ manag_game.py
@@ -1,23 +1,24 @@
 from board_check import all_checks,boards_outputs
 from board import print_board, create_board,input_explain
-from play_movement import is_free, get_index, mark_board
+from play_movement import is_free, get_index, mark_board,get_simbol
 from players import ask_player_names
 
 def run_game():
     name1,name2 = ask_player_names()
+    sign_1,sign_2=get_simbol(name1,name2)
     input_explain(name1)
     board = create_board ()
     player = 1
 
     for turn in range (9):
         
-        row, column = get_index(player, name1, name2)
+        row, column = get_index(player, name1,name2)
     
         if is_free(board,row,column):
             if player % 2 != 0:
-                mark_board(board,row,column,'X')
+                mark_board(board,row,column,sign_1)
             else:
-                mark_board(board,row,column,'O')
+                mark_board(board,row,column,sign_2)
             print_board(board)
 
         else:
