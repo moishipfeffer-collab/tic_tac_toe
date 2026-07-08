e08bb1cc84ed36f8ed9717a945cb34b4ad55c0e7
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-08 10:39:12 +0300

    indentation change

--- manag_game.py
+++ manag_game.py
@@ -11,19 +11,19 @@ def run_game():
 
     for turn in range (9):
         
-                row, column = get_index(player, name1, name2)
+        row, column = get_index(player, name1, name2)
     
-                if is_free(board,row,column):
-                    if player % 2 != 0:
-                        mark_board(board,row,column,'X')
-                    else:
-                        mark_board(board,row,column,'O')
-                    print_board(board)
+        if is_free(board,row,column):
+            if player % 2 != 0:
+                mark_board(board,row,column,'X')
+            else:
+                mark_board(board,row,column,'O')
+            print_board(board)
 
-                else:
-                    print ("that index wasn't free") 
-                    turn-=1
-                    continue
+        else:
+            print ("that index wasn't free") 
+            turn-=1
+            continue
 
             
         if all_checks(board):
